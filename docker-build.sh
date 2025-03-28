0c596dc084d14d93e4e115ab414ad269c449b71e
Y!an <i@yian.me>
2025-03-28 17:58:16 +0800

    update build script

--- docker-build.sh
+++ docker-build.sh
@@ -17,3 +17,10 @@ else
 fi
 
 DOCKER_BUILDKIT=1 docker build -t $IMG_NAME:latest -t $IMG_NAME:$VERSION .
+
+# 按创建时间保留最近 7 个版本
+docker images --filter "reference=$IMG_NAME" --format "{{.ID}} {{.CreatedAt}} {{.Tag}}" \
+  | grep -v "latest" \
+  | sort -k2 -r \
+  | awk 'NR>7 {print $1 " " $3}' \
+  | xargs -r -n2 sh -c 'docker rmi $0:$1' 2>/dev/null
